0174d120676aa32ae3fae6280c0e6ec67ed83922
Imran Shaikh <shaikhimran65625@gmail.com>
2025-09-07 16:14:52 +0530

    Fix: corrected datauri import path

--- utils/dataUri.js
+++ utils/dataUri.js
@@ -1,8 +1,10 @@
-import DataURIParser from "dataUri/Parser.js"
-import path from "path"
-const getDataUri=(file)=>{
-    const parser =new DataURIParser()
-    const extname=path.extname(file.originalname).toString()
-    return parser.format(extname, file.buffer)
-}
-export default getDataUri
+import DataURIParser from "datauri/parser.js";
+import path from "path";
+
+const getDataUri = (file) => {
+    const parser = new DataURIParser();
+    const extname = path.extname(file.originalname).toString();
+    return parser.format(extname, file.buffer);
+};
+
+export default getDataUri;
